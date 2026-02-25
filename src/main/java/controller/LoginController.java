93074260b9f685073234b0c97e069ced8692a351
Tran Viet <tranducviet.tv@gmail.com>
2026-02-25 22:58:46 +0700

    bo sung tinh nang login

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import dal.UserAccountDAO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import entity.UserAccounts;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(name="LoginController", urlPatterns={"/login"})
+public class LoginController extends HttpServlet {
+    
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("PRJ301Proj1_PU");;
+    private EntityManager em = emf.createEntityManager();
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UserAccountDAO userDAO = new UserAccountDAO(em);
+        UserAccounts user = userDAO.findByUsernameAndPassword(username, password);
+        
+        if(user != null && user.getPassword().equals(password)){
+            HttpSession loginSession = request.getSession();
+            loginSession.setAttribute("account", user);
+            response.sendRedirect("index.html");
+        }else{
+            request.setAttribute("msg", "Dang nhap khong thanh cong");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,29 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%-- 
+    Document   : login
+    Created on : Feb 25, 2026, 7:14:04 PM
+    Author     : tranv
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login Page</h1>
+        <form action="login" method="POST">
+            Username <input type="text" name="username" value="" /> <br>
+            Password <input type="password" name="password" value="" /> <br>
+            <c:if test="${not empty msg}">
+                <div> 
+                    ${msg}
+                </div>
+            </c:if>
+            <input type="submit" value="Login" name="Login" />
+        </form>
+        
+    </body>
+</html>
